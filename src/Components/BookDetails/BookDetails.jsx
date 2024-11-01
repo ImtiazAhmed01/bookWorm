6d9829f08489d9f9f0e0bf4c7c8f3c97f225a850
ImtiazAhmed01 <imtiazahm21@gmail.com>
2024-11-01 11:52:47 -0700

    book added

---
+++ src/Components/BookDetails/BookDetails.jsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+const BookDetails = () => {
+}
+aram = usePrams();
+return (
+    <div>
+        <h1></h1>
+        S3
+    </div>
+);
+};
+
+export default BookDetails;
