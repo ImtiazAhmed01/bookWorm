723d1ee523841cf22eb6e6a239b5475a2e2f7ac3
ImtiazAhmed01 <imtiazahm21@gmail.com>
2024-11-01 06:51:24 -0700

    book added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,11 @@
+import Banner from "../Banner/Banner";
+import Books from "../Book/Books";
 
 const Home = () => {
     return (
         <div>
-            <h2 className='text-4xl'>This is home page</h2>
+            <Banner></Banner>
+            <Books></Books>
         </div>
     );
 };
